6b295bb01f93763624d45fefdc5ff41123bcc795
AzumaOkumura <azmok@users.noreply.github.com>
2021-02-20 00:26:59 +0900

    symbol input added

--- wordpress/wp-content/plugins/azmok_change_excerpt_length/azmok_change_excerpt_length.php
+++ wordpress/wp-content/plugins/azmok_change_excerpt_length/azmok_change_excerpt_length.php
@@ -13,34 +13,47 @@
 ##################
 
 ### register, render settings title, field
-function render_excerptChanger_title( $args){
+function render_excerptChanger_section($args){
   echo "<u><i>current length:</i></u>";
 }
-function render_excerptChanger_field( $args){
+function render_excerptChanger_lengthField($args){
    $val = get_option('azmok_excerptChanger');
    $expr = isset( $val ) ? esc_attr( $val ) : "";
-   $html = "<input type='text' placeholder='Enter Length' name='azmok_excerptChanger' value='{$expr}' />";
+   $html = "<input type='text' placeholder='Enter Length' name='azmok_excerptChanger-length-field' value='{$expr}' />";
    ##
-   // 'name attribute must match against $option_name of 'register_setting($page, $option_name)
+   // 'name attribute must match against $option_name of 'register_setting($page, $option_$name)
    //   because it automatically identify options value and saved to DB, and enable to
    //   use 'get_option()' to retrieve the custome option's value. 
    
    echo $html;
 }
 
+function render_excerptChanger_symbolField($args){
+   $symbol = get_option('azmok_excerptChanger-symbol-field');
+   $expr = isset( $symbol ) ? esc_attr( $symbol ) : "...";
+   $html = "<input type='text' placeholder='Enter 
+      symbol' name='azmok_excerptChanger-symbol-field' value='{$expr}' />";
+   
+   echo $html;
+}
+
 function register_excerptChanger(){
+   
+   ################
+   # 'length' for excerpt
+   ################
    ###  Register custom option
-   register_setting('reading', 'azmok_excerptChanger');
+   register_setting('reading', 'azmok_excerptChanger-length-field');
    //                $page   ,  $option_name
    
    ###  add Section
    add_settings_section(
-      'excerptChanger-section', // in HTML, this value used as 'section's id attribute value 
+      'azmok_excerptChanger-section', // in HTML, this value used as 'section's id attribute value 
                                 //   In addition, this is used as identifier in PHP.
       
-      'Change Excerpt Length',  // section title in HTML.
+      'Change Excerpt',  // section title in HTML.
       
-      'render_excerptChanger_title', // name of callback function to render HTML.
+      'render_excerptChanger_section', // name of callback function to render HTML.
       
       'reading' // built-in settings pages name('general', 'discussion', 'media', 'reading',
                 //   'writing', 'misc', 'options', 'privacy')
@@ -48,26 +61,45 @@ function register_excerptChanger(){
    
    ###  add Field
    add_settings_field(
-      'excerptChanger-field',
+      'azmok_excerptChanger-length-field',
          
       'Current Excerpt Length',
-      'render_excerptChanger_field',
+      'render_excerptChanger_lengthField',
       'reading',
-      'excerptChanger-section' // parent section id in order to append this field. If not specified, this field will append to 'default' section.
+      'azmok_excerptChanger-section' // parent section id in order to append this field. If not specified, this field will append to 'default' section.
    );
+   
+   
+   
+   
+   
+   ################
+   # 'Symbol' for Excerpt
+   ################
+   register_setting('reading', 'azmok_excerptChanger-symbol-field');
+   
+   add_settings_field(
+      'azmok_excerptChanger-symbol-field',
+      'Current symbol',
+      'render_excerptChanger_symbolField',
+      'reading',
+      'azmok_excerptChanger-section'
+   )
 }
 add_action('admin_init', 'register_excerptChanger');
 
 
 
+function azmok_registerExcerptChanger_
 
 
 ##################
 # (Frontend)
 # - chnage excerpt length using DB data(user inputted)
 ##################
-function azmok_changeExcerptLength( $excerpt ){
-   $optionsLen = get_option('azmok_excerptChanger');
+function azmok_changeExcerpt( $excerpt ){
+   $optionsLen = get_option('azmok_excerptChanger-length-field');
+   $optionsSymbol = get_option('azmok_excerptChanger-symbol-field');
    $excerptLen = mb_strlen($excerpt);
    
    // echo '<br>$excerpt<br>'.  htmlspecialchars($excerpt);
@@ -88,8 +120,8 @@ function azmok_changeExcerptLength( $excerpt ){
    # nre-surfix tags previously stripped 
    #
    
-   return "<p>{$newExcerpt}</p>";
+   return "<p>{$newExcerpt}{$optionsSymbol}</p>";
 }
    
    
-add_filter('the_excerpt', 'azmok_changeExcerptLength', 999);
+add_filter('the_excerpt', 'azmok_changeExcerpt', 999);
